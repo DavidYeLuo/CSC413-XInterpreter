2a10233d530119ba4ba043d80d73d0423084ddaf
Student <davidayeluo@gmail.com>
2022-06-23 16:59:46 -0700

    Added implementation based on abstraction ideas (unable to test it yet).

--- interpreter/virtualmachine/Program.java
+++ interpreter/virtualmachine/Program.java
@@ -2,18 +2,22 @@ package interpreter.virtualmachine;
 
 import interpreter.bytecode.ByteCode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class Program {
+public class Program
+{
 
-    private List<ByteCode> program;
+    private List<ByteCode>    program;
+    private LabelToAddressMap labelToAddressMap;
 
-    public Program() {
-        program = new ArrayList<>();
+    public Program()
+    {
+        program           = new ArrayList<>();
+        labelToAddressMap = new LabelToAddressMap();
     }
 
-    protected ByteCode getCode(int programCounter) {
+    protected ByteCode getCode(int programCounter)
+    {
         return this.program.get(programCounter);
     }
 
@@ -24,13 +28,68 @@ public class Program {
      * HINT: make note what type of data-structure ByteCodes are stored in.
      * **** METHOD SIGNATURE CANNOT BE CAHNGED *****
      */
-    public void resolveAddress() {
+    public void resolveAddress()
+    {
+        // We're using a stack to save us from needing to loop through the program again.
+        Stack<ByteCode> jumpableStack = new Stack<>();
 
+        for (ByteCode byteCode : program)
+        {
+            // 1st pass through the arrayList keeping track of label codes and their labels.
+            if (byteCode.getClass() == LabelCode.class) // Only allow labels
+            {
+                // Need to grab label from the ByteCode
+                labelToAddressMap.add(byteCode.getLabel());
+            }
+
+            // Keep track of gotoByteCode to resolve it later.
+            if (byteCode instanceof JumpCode)
+            {
+                jumpableStack.add(byteCode);
+            }
+        }
+        // 2nd pass through the arrayList look for call, goto and falsebranch codes
+        // and do the following:
+        //  Look at stored label codes and find the 1 that has the match label value.
+
+        // Set the address of from the bytecode to the address retrieved from our map
+        ByteCode byteCode;
+        int      address;
+        while (!jumpableStack.isEmpty())
+        {
+            byteCode = jumpableStack.pop();
+            address  = labelToAddressMap.get(byteCode.getLabel());
+            byteCode.setAddress(address);
+        }
     }
 
 
     public void add(ByteCode bc)
     {
-        System.out.println("TODO: Implement add method in Program class");
+        program.add(bc);
+    }
+
+    private class LabelToAddressMap
+    {
+        private Map<String, Integer> map;
+        private int                  latestProgramCounter;
+
+        LabelToAddressMap()
+        {
+            map                  = new HashMap<>();
+            latestProgramCounter = 0;
+        }
+
+        public void add(String label)
+        {
+            map.put(label, latestProgramCounter);
+            latestProgramCounter++;
+        }
+
+        public int get(String label)
+        {
+            return map.get(label);
+        }
     }
 }
+
