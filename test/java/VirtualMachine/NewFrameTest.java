5ab1e9bbda22ea103c96318b2758c60420eea561
Student <davidayeluo@gmail.com>
2022-06-27 11:54:28 -0700

    Added test for making new frames.

---
+++ test/java/VirtualMachine/NewFrameTest.java
@@ -0,0 +1,99 @@
+package VirtualMachine;
+
+import interpreter.virtualmachine.Program;
+import interpreter.virtualmachine.VirtualMachine;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import test.TestHelper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("VirtualMachine_StoreTest")
+public class NewFrameTest
+{
+    private VirtualMachine virtualMachine;
+
+    // Init is called in every test
+    @BeforeEach
+    void init()
+    {
+        virtualMachine = new VirtualMachine(new Program());
+    }
+
+    @AfterEach
+    void tearDown() {virtualMachine = null;}
+
+    @Test
+    void pushTenItems_newFrameAtZero_popFrame()
+    {
+        TestHelper.pushNStack(virtualMachine, 10);
+
+        virtualMachine.newFrameAt(0);
+
+        assertEquals(10, virtualMachine.pop(VirtualMachine.POP_FRAME));
+    }
+
+    @Test
+    void newState_popFrame()
+    {
+        assertEquals(0, virtualMachine.pop(VirtualMachine.POP_FRAME));
+    }
+
+    @Test
+    void newState_PopTwice()
+    {
+        assertEquals(0, virtualMachine.pop(VirtualMachine.POP_FRAME));
+        assertEquals(0, virtualMachine.pop(VirtualMachine.POP_FRAME));
+    }
+
+    @Test
+    void pushTenItems_newFrameAtThree_popFrame()
+    {
+        TestHelper.pushNStack(virtualMachine, 10);
+
+        virtualMachine.newFrameAt(3);
+
+        assertEquals(7, virtualMachine.pop(VirtualMachine.POP_FRAME));
+    }
+
+    @Test
+    void pushTenItems_newFrameAtNine_popFrame()
+    {
+        TestHelper.pushNStack(virtualMachine, 10);
+
+        virtualMachine.newFrameAt(9);
+
+        assertEquals(1, virtualMachine.pop(VirtualMachine.POP_FRAME));
+    }
+
+    @Test
+    void pushTenItems_newFrameAtTen_popFrame()
+    {
+        TestHelper.pushNStack(virtualMachine, 10);
+
+        virtualMachine.newFrameAt(10);
+
+        assertEquals(1, virtualMachine.pop(VirtualMachine.POP_FRAME));
+    }
+
+    @Test
+    void pushTenItems_newFrameAtOneHundred_popFrame()
+    {
+        TestHelper.pushNStack(virtualMachine, 10);
+
+        virtualMachine.newFrameAt(100);
+
+        assertEquals(1, virtualMachine.pop(VirtualMachine.POP_FRAME));
+    }
+    @Test
+    void pushTenItems_newFrameAtNegativeTen_popFrame()
+    {
+        TestHelper.pushNStack(virtualMachine, 10);
+
+        virtualMachine.newFrameAt(-10);
+
+        assertEquals(10, virtualMachine.pop(VirtualMachine.POP_FRAME));
+    }
+}
