616ce770bf23e5efd6c648db74184a0f90bb705f
Student <davidayeluo@gmail.com>
2022-06-28 16:46:27 -0700

    Added Goto dump.

--- interpreter/bytecode/GotoCode.java
+++ interpreter/bytecode/GotoCode.java
@@ -2,11 +2,17 @@ package interpreter.bytecode;
 
 import interpreter.virtualmachine.VirtualMachine;
 
-public class GotoCode extends JumpCode
+public class GotoCode extends JumpCode implements Dumpable
 {
     @Override
     public void execute(VirtualMachine virtualMachine)
     {
         virtualMachine.setProgramCounter(getAddress());
+    }
+
+    @Override
+    public String dump()
+    {
+        return "GOTO " + getLabel();
     }
 }
