86293f954969e55833c8726e557316cf72284b01
Student <davidayeluo@gmail.com>
2022-06-29 12:18:37 -0700

    Removed debug statements for false branch bytecode.

--- interpreter/bytecode/FalseBranchCode.java
+++ interpreter/bytecode/FalseBranchCode.java
@@ -10,11 +10,9 @@ public class FalseBranchCode extends JumpCode implements Dumpable
         int topStack = virtualMachine.pop(1);
         if(topStack == VirtualMachine.ERROR_RETURN_CODE)
         {
-//            System.out.println("ERROR: top of the stack is empty");
             return;
         }
         if(topStack == 0) virtualMachine.setProgramCounter(getAddress());
-        if(topStack != 1) System.out.println("Error: value not expected");
     }
 
 
